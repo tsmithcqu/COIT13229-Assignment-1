e4dedfe4c617111c6cebe66c5cc0dacac05e6008
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-03-10 20:32:22 +1000

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -1,1 +1,19 @@
-Version 1
+package FitnessClub;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class TCPClient {
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+Socket socket = new Socket("localhost", 2245);
+
+while(true) {
+System.out.println("Enter member name or 'quit' to exit:");
+String name = scanner.nextLine();
+			
+//Need to make this neater. Need to ask for first name and last name and address sweparately, and add so the user can change if needed. 
+        }
+    }
+}
