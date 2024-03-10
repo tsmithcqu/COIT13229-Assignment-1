ed23be75d216338a695289137b22f5a30da46286
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-03-10 20:32:04 +1000

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,1 +1,25 @@
-Verion 1! Hello world
+package FitnessClub; 
+
+public class Member{
+private String name;
+private String address;
+private String phoneNumber;
+
+public Member() {
+}
+
+public Member(String name, String address, String phoneNumber) {
+}
+
+public String getName() {
+return name;
+}
+
+public String getAddress() {
+return address;
+}
+
+public String getPhoneNumber() {
+return phoneNumber;
+}
+}
