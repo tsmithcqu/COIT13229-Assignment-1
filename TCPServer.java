340cad095ba4b36147b4b46916367a5c5da5a750
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-03-10 20:33:56 +1000

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -1,1 +1,16 @@
-Version 1
+package FitnessClub; 
+
+import java.io.*;
+import java.net.*;
+
+public class TCPServer {
+public static void main(String[] args) throws IOException {
+ServerSocket welcomeSocket = new ServerSocket(2245);
+
+while (true) {
+Socket connectionSocket = welcomeSocket.accept();
+}
+}
+}
+
+// Confirmation message? Not sure where it goes. Investigate tomorrow. 
