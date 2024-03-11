a37e2b86baa7a8e696863e176fb8e2c8eb67ef8c
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-03-11 20:37:16 +1000

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -4,11 +4,17 @@ import java.io.*;
 import java.net.*;
 
 public class TCPServer {
-public static void main(String[] args) throws IOException {
+public static void main(String[] args){
 ServerSocket welcomeSocket = new ServerSocket(2245);
 
 while (true) {
 Socket connectionSocket = welcomeSocket.accept();
+Connection c = new Connection(clientSocket);
+System.out.printf("\nServer waiting on: %d for client from %d ",
+listenSocket.getLocalPort(), clientSocket.getPort() );
+}
+catch(IOException e){
+System.out.println("Listen :"+e.getMessage());
 }
 }
 }
