f84b30cbf6c8a3c8c35e54ee10b2b91ec8e29450
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-03-18 21:02:19 +1000

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -16,6 +16,13 @@ public class TCPClient {
             socket = new Socket(hostName, serverPort); // Establishing connection to the server.
             scanner = new Scanner(System.in); // Initialising the scanner to read from system's input.
 
+		while (true) {
+                System.out.println("Enter member name or 'quit' to exit:"); // Prompt user to enter text.
+                String name = scanner.nextLine();
+                if (name.equalsIgnoreCase("quit")) { // If the user types "quit", exit the program. 
+                    break; 
+                }
+			
 		// Handle exceptions related to the host of the server being unknown.
         } catch (UnknownHostException e) {
             System.out.println("Sock:" + e.getMessage()); 
