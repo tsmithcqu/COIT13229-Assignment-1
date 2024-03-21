ac96e2a9e47acc47a46f135330385f2f190b8b96
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-03-21 13:18:54 +1000

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,25 +1,51 @@
-package FitnessClub; 
+package FitnessClub;
 
-public class Member{
-private String name;
-private String address;
-private String phoneNumber;
+import java.io.Serializable;
 
-public Member() {
-}
+public class Member implements Serializable {
+    
+    // USed to store the member's information.
+    private String name;       
+    private String address;    
+    private String phoneNumber; 
 
-public Member(String name, String address, String phoneNumber) {
-}
+    public Member() {
+    }
 
-public String getName() {
-return name;
-}
+    // Creates a new instance with the provided name, address, and phone number.
+    public Member(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
 
-public String getAddress() {
-return address;
-}
+    // Gets the name of the member.
+    public String getName() {
+        return name;
+    }
 
-public String getPhoneNumber() {
-return phoneNumber;
-}
+    // Sets the name of the member.
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Gets the address of the member.
+    public String getAddress() {
+        return address;
+    }
+
+    // Sets the address of the member.
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Gets the phone number of the member.
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Sets the phone number of the member.
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
