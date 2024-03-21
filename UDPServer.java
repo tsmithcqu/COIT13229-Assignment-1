f85eb05fb5243ba785b46786aac1a9a5cab0b2de
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-03-21 20:54:20 +1000

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -1,1 +1,10 @@
-Version 1
+import java.net.*;
+
+public class UDPServer {
+    public static void main(String args[]) {
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket(2245); // Create a UDP socket bound to the specified port. 
+        }
+    }
+}
