db00e27c1e726d27e2760e0faba5f44f8024d134
tsmithcqu <130130661+tsmithcqu@users.noreply.github.com>
2024-03-30 19:13:20 +1000

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -3,6 +3,8 @@ package FitnessClub;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TCPClient {
 
